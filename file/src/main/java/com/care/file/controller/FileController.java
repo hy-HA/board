6eb31aa6e3669676faa45c866950a74d36ae761d
hy-HA <heeyoung-ha@naver.com>
2022-04-23 13:42:57 +0900

    download part2

--- file/src/main/java/com/care/file/controller/FileController.java
+++ file/src/main/java/com/care/file/controller/FileController.java
@@ -1,11 +1,19 @@
 package com.care.file.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -40,6 +48,16 @@ public class FileController {
    public String views(Model model) {
 	   fs.getImages(model);
 	   return "result";
+   }
+   @GetMapping("download")
+   public void download(@RequestParam String file,
+		   			HttpServletResponse response) throws Exception {
+	   response.addHeader("Content-disposition", 
+			   	"attachment; fileName="+file);
+	   File fi = new File(FileService.IMAGE_REPO+"/"+file);
+	   FileInputStream in = new FileInputStream(fi);
+	   FileCopyUtils.copy(in, response.getOutputStream());
+	   in.close();
    }
 }
 
--- file/src/main/webapp/WEB-INF/views/result.jsp
+++ file/src/main/webapp/WEB-INF/views/result.jsp
@@ -1,21 +1,27 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
+<meta charset="UTF-8">
+<title>Insert title here</title>
 </head>
 <body>
-<%@ taglib prefix="c" 
-			uri="http://java.sun.com/jsp/jstl/core" %>
-<c:set var ="contextPath"
-		value="${pageContext.request.contextPath }" />
-<c:forEach var="dto" items="${list }">
-	아이디 : ${dto.id}<br>
-	이름 : ${dto.name}<br>
-	이미지 : ${dto.imageName}<hr>
-</c:forEach>
-<a href="${contextPath }/form">업로드 이동</a>
+   <%@ taglib uri="http://java.sun.com/jsp/jstl/core" 
+                                 prefix="c" %>
+   <c:set var="contextPath" 
+      value="${pageContext.request.contextPath }"/>
+   <c:forEach var="dto" items="${list }">
+      아이디 : ${dto.id }<br>
+      이름 : ${dto.name }<br>
+      이미지 : ${dto.imageName }
+      <img src="${contextPath }/download?file=${dto.imageName}" width="150px" height="150px">
+<a href="${contextPath }/download?file=${dto.imageName}">
+   ${dto.imageName} 다운받기
+</a>
+      <hr>
+   </c:forEach>
+   <a href="${contextPath }/form">업로드 이동</a>
 
 
 </body>
